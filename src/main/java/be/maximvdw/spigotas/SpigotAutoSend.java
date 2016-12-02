8fe09fb7dcfa56d1aceac8aaf43cdbe5cc2bed44
Maxim Van de Wynckel <maxim.vandewynckel@gmail.com>
2016-12-02 17:24:07 +0100

    more logging

--- src/main/java/be/maximvdw/spigotas/SpigotAutoSend.java
+++ src/main/java/be/maximvdw/spigotas/SpigotAutoSend.java
@@ -18,191 +18,197 @@ import java.util.*;
 
 /**
  * Spigot Auto Reply
- * 
+ *
  * @author Maxim Van de Wynckel
  */
 public class SpigotAutoSend {
-	/* Authenticated user */
-	private User user = null;
-	private User conversationUser = null;
-	private HashMap<Resource, List<User>> buyers = new HashMap<Resource, List<User>>();
-	private boolean running = true;
-	private LogStorage log = null;
+    /* Authenticated user */
+    private User user = null;
+    private User conversationUser = null;
+    private HashMap<Resource, List<User>> buyers = new HashMap<Resource, List<User>>();
+    private boolean running = true;
+    private LogStorage log = null;
 
-	@SuppressWarnings("deprecation")
-	public SpigotAutoSend(String... args) {
-		Console.info("Initializing Spigot Auto Send ...");
-		new SpigotSiteCore();
-		new Configuration(2); // Version 2
+    @SuppressWarnings("deprecation")
+    public SpigotAutoSend(String... args) {
+        Console.info("Initializing Spigot Auto Send ...");
+        new SpigotSiteCore();
+        new Configuration(2); // Version 2
 
-		String username = Configuration.getString("username");
-		String password = Configuration.getString("password");
-		String totpSecret = Configuration.getString("2fakey");
-		final int interval = Configuration.getInt("interval");
+        String username = Configuration.getString("username");
+        String password = Configuration.getString("password");
+        String totpSecret = Configuration.getString("2fakey");
+        final int interval = Configuration.getInt("interval");
 
-		Console.info("Logging in " + username + " ...");
-		try {
-			User user = SpigotSite.getAPI().getUserManager()
-					.authenticate(username, password,totpSecret);
-			setUser(user);
-		} catch (InvalidCredentialsException e) {
-			Console.info("Unable to log in! Wrong credentials!");
-			return;
-		} catch (TwoFactorAuthenticationException e) {
+        Console.info("Logging in " + username + " ...");
+        try {
+            User user = SpigotSite.getAPI().getUserManager()
+                    .authenticate(username, password, totpSecret);
+            setUser(user);
+        } catch (InvalidCredentialsException e) {
+            Console.info("Unable to log in! Wrong credentials!");
+            return;
+        } catch (TwoFactorAuthenticationException e) {
             Console.info("Unable to log in! Two factor authentication failed!");
             return;
         }
         password = null;
         totpSecret = null;
 
-		Console.info("Getting your premium plugins ...");
-		if (user == null)
-			return;
-		List<Resource> resources = SpigotSite.getAPI().getResourceManager()
-				.getResourcesByUser(user);
-		for (Resource res : resources) {
-			if (res instanceof PremiumResource) {
-				Console.info("\t" + res.getResourceName());
-				try {
-					buyers.put(
-							res,
-							SpigotSite
-									.getAPI()
-									.getResourceManager()
-									.getPremiumResourceBuyers(
-											(PremiumResource) res, user));
-				} catch (ConnectionFailedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					return;
-				}
-			}
-		}
+        Console.info("Getting your premium plugins ...");
+        if (user == null)
+            return;
+        List<Resource> resources = SpigotSite.getAPI().getResourceManager()
+                .getResourcesByUser(user);
+        for (Resource res : resources) {
+            if (res instanceof PremiumResource) {
+                Console.info("\t" + res.getResourceName());
+                try {
+                    List<User> resourceBuyers = SpigotSite
+                            .getAPI()
+                            .getResourceManager()
+                            .getPremiumResourceBuyers(
+                                    (PremiumResource) res, user);
+                    buyers.put(
+                            res,
+                            resourceBuyers);
+                    Console.info("\t\tBuyers: " + resourceBuyers.size());
+                } catch (ConnectionFailedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        }
 
-		if (buyers.isEmpty()) {
-			Console.info("You do not have any premium plugins!");
-			return;
-		}
+        if (buyers.isEmpty()) {
+            Console.info("You do not have any premium plugins!");
+            return;
+        }
 
-		log = new LogStorage("log");
-		logMessage("Starting now ...");
+        log = new LogStorage("log");
+        logMessage("Starting now ...");
 
-		Thread th = new Thread(new Runnable() {
+        Thread th = new Thread(new Runnable() {
 
-			public void run() {
-				while (running) {
-					try {
-						Thread.sleep(interval * 1000);
-						boolean hasSend = false;
-						for (Resource res : buyers.keySet()) {
-							Console.info("Checking for new buyers in: "
-									+ res.getResourceName());
-							List<User> newBuyers = SpigotSite
-									.getAPI()
-									.getResourceManager()
-									.getPremiumResourceBuyers(
-											(PremiumResource) res, user);
-							if (newBuyers.isEmpty() && (!buyers.isEmpty()))
-								continue;
-							for (User newBuyer : newBuyers) {
-								if (!buyers.get(res).contains(newBuyer)) {
-									Set<String> recipients = new HashSet<String>();
-									recipients.add(newBuyer.getUsername());
-									Console.info("Sending a message to "
-											+ newBuyer.getUsername());
+            public void run() {
+                while (running) {
+                    try {
+                        Thread.sleep(interval * 1000);
+                        boolean hasSend = false;
+                        for (Resource res : buyers.keySet()) {
+                            Console.info("Checking for new buyers in: "
+                                    + res.getResourceName());
+                            List<User> newBuyers = SpigotSite
+                                    .getAPI()
+                                    .getResourceManager()
+                                    .getPremiumResourceBuyers(
+                                            (PremiumResource) res, user);
+                            Console.info("\t\tBuyers: " + newBuyers.size());
+                            if (newBuyers.isEmpty() && (!buyers.isEmpty()))
+                                continue;
+                            for (User newBuyer : newBuyers) {
+                                if (!buyers.get(res).contains(newBuyer)) {
+                                    Set<String> recipients = new HashSet<String>();
+                                    recipients.add(newBuyer.getUsername());
+                                    Console.info("Sending a message to "
+                                            + newBuyer.getUsername());
 
-									if (hasSend) {
-										Console.info("Waiting before sending to prevent spam ...");
-										Thread.sleep(15000);
-										hasSend = false;
-									}
-									String title = Configuration
-											.getString("title")
-											.replace("{plugin}",
-													res.getResourceName())
-											.replace("{member}",
-													newBuyer.getUsername());
-									String message = Configuration
-											.getString("message")
-											.replace("{plugin}",
-													res.getResourceName())
-											.replace("{member}",
-													newBuyer.getUsername());
+                                    if (hasSend) {
+                                        Console.info("Waiting before sending to prevent spam ...");
+                                        Thread.sleep(15000);
+                                        hasSend = false;
+                                    }
+                                    String title = Configuration
+                                            .getString("title")
+                                            .replace("{plugin}",
+                                                    res.getResourceName())
+                                            .replace("{member}",
+                                                    newBuyer.getUsername());
+                                    String message = Configuration
+                                            .getString("message")
+                                            .replace("{plugin}",
+                                                    res.getResourceName())
+                                            .replace("{member}",
+                                                    newBuyer.getUsername());
 
-									logMessage("[BUYER] "
-											+ newBuyer.getUsername() + " ["
-											+ newBuyer.getUserId()
-											+ "] bought "
-											+ res.getResourceName());
+                                    logMessage("[BUYER] "
+                                            + newBuyer.getUsername() + " ["
+                                            + newBuyer.getUserId()
+                                            + "] bought "
+                                            + res.getResourceName());
 
-									if (!Configuration.getBoolean("debug"))
-										try {
-											SpigotSite
-													.getAPI()
-													.getConversationManager()
-													.createConversation(
-															user,
-															recipients,
-															title,
-															message,
-															Configuration
-																	.getBoolean("options.lock"),
-															false, false);
-										} catch (SpamWarningException ex) {
-											Console.info("Spamming detected! ... Waiting for the next interval!");
-											continue;
-										}
+                                    if (!Configuration.getBoolean("debug")) {
+                                        try {
+                                            SpigotSite
+                                                    .getAPI()
+                                                    .getConversationManager()
+                                                    .createConversation(
+                                                            user,
+                                                            recipients,
+                                                            title,
+                                                            message,
+                                                            Configuration
+                                                                    .getBoolean("options.lock"),
+                                                            false, false);
+                                        } catch (SpamWarningException ex) {
+                                            Console.info("Spamming detected! ... Waiting for the next interval!");
+                                            continue;
+                                        }
+                                    } else {
+                                        Console.info("^ Above message was not send due to debug mode!");
+                                    }
 
-									hasSend = true;
-								}
+                                    hasSend = true;
+                                }
 
-							}
+                            }
 
-							buyers.put(res, newBuyers);
-						}
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-				}
-			}
+                            buyers.put(res, newBuyers);
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
 
-		});
-		th.start();
-		Console.info("Press ENTER to quit.");
-		System.console().readLine();
-		th.stop();
-		running = false;
-	}
+        });
+        th.start();
+        Console.info("Press ENTER to quit.");
+        System.console().readLine();
+        th.stop();
+        running = false;
+    }
 
-	private void logMessage(String message) {
-		Date today;
-		String output;
-		SimpleDateFormat formatter;
+    private void logMessage(String message) {
+        Date today;
+        String output;
+        SimpleDateFormat formatter;
 
-		formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.ENGLISH);
-		today = new Date();
-		output = formatter.format(today);
-		String resultMessage = "[" + output + "] " + message;
-		log.appendLog(resultMessage);
-	}
+        formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.ENGLISH);
+        today = new Date();
+        output = formatter.format(today);
+        String resultMessage = "[" + output + "] " + message;
+        log.appendLog(resultMessage);
+    }
 
-	public static void main(String... args) {
-		new SpigotAutoSend(args);
-	}
+    public static void main(String... args) {
+        new SpigotAutoSend(args);
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	private void setUser(User user) {
-		this.user = user;
-	}
+    private void setUser(User user) {
+        this.user = user;
+    }
 
-	public User getConversationUser() {
-		return conversationUser;
-	}
+    public User getConversationUser() {
+        return conversationUser;
+    }
 
-	public void setConversationUser(User conversationUser) {
-		this.conversationUser = conversationUser;
-	}
+    public void setConversationUser(User conversationUser) {
+        this.conversationUser = conversationUser;
+    }
 }
